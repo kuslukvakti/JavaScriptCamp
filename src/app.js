a3f2e0c19e185c84790d339f5e78095d71ceca39
kuslukvakti <kusluk.vakti@gmail.com>
2021-11-01 20:04:53 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+console.log("Merhaba Kodlama IOcccc")
+
+// JS type safe degildir
+
+let dolarDun = 9.30
+
+dolarDun = "9.20"
+
+{
+    let dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+
+//euroDun = 11
+
+console.log(euroDun)
+
+//---------------------------------------------
+let konutKredileri = ["a","b","c"]
+
+console.log("<ul>")
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>");
+}
+
+console.log("</ul>")
+
+
